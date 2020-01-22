fda5aaad3af5c0de192df046f2897e81ae918f79
bigtonito39 <bigtonito@hotmail.com>
2020-01-21 19:41:37 -0500

    folders created, as well linking them to routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import Login from "./components/Login";
+import ListOfFriends from "./components/ListOfFriends";
+import AddAfriend from "./components/AddAfriend";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+   <Router>
+<div className="App">
+<div className="NavDiv">
+<Link to="/login" className="nav">Login</Link>
+<Link to="/protected" className="nav">Friends</Link>
+<Link to="/newfriend" className="nav">Add a friend</Link>
+</div>
+<Switch>
+  <ProtectedRoute exact path="/protected" component={ListOfFriends}/>
+  <Route path="/login" component={Login}/>
+  <Route path="/newfriend" component={AddAfriend} />
+</Switch>
+</div>
+
+   </Router>
   );
 }
 
