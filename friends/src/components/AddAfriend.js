705f7bbbb3c6627a1e9502847d1f754d760eef1d
bigtonito39 <bigtonito@hotmail.com>
2020-01-21 20:30:11 -0500

    AddAfriend Component Created, in this component i created a form to send input to state, then i pass the state through a post request to the server, token needed to be passed so that server would give me access to do so

---
+++ friends/src/components/AddAfriend.js
@@ -0,0 +1,67 @@
+import React from "react";
+import axios from "axios";
+
+class AddAfriend extends React.Component {
+    state = {
+        newFriends:{
+            name:"",
+            age:"",
+            email:""
+
+        }
+        }
+
+        handleChanges =event =>{
+            this.state({
+                newFriends: {
+               ...this.state.newFriends,[event.target.name]:event.target.value
+                }
+            })
+        }
+
+        addnewFriend = event => {
+            axios.post("http://localhost:5000/api/friends", this.state.newFriends, {
+                headers:{Authorization:localStorage.getItem("token")}
+            })
+            .then(response => {console.log("response from adding a friend", response)})
+            .catch(err => {console.log("error from adding a friend", err)})
+        }
+
+        render() {
+            return (
+                <div>
+                    <h1>Add your new friend</h1>
+                    <form onSubmit ={this.addnewFriend}>
+                        <input 
+                        type="text"
+                        name="name"
+                        value={this.state.newFriends.name}
+                        onChange={this.handleChanges}
+                        placeholder="Full name goes here please"
+                        
+                        />
+                          <input 
+                        type="text"
+                        name="age"
+                        value={this.state.newFriends.age}
+                        onChange={this.handleChanges}
+                        placeholder="Age goes here please"
+                        
+                        />
+                         <input 
+                        type="text"
+                        name="email"
+                        value={this.state.newFriends.email}
+                        onChange={this.handleChanges}
+                        placeholder="Email goes here please"
+                        
+                        />
+
+                        <button>Add your friend Now!</button>
+
+                        
+                    </form>
+                </div>
+            )
+        }
+}
